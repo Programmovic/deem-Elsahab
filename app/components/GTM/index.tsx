c231ad63e708a97e7d62d68f054a6596512e4a08
Adam Hasan Said <91834955+Programmovic@users.noreply.github.com>
2025-05-24 22:49:16 +0300

    Update index.tsx

--- app/components/GTM/index.tsx
+++ app/components/GTM/index.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useEffect } from 'react';
@@ -7,16 +6,18 @@ export default function GTM() {
   useEffect(() => {
     if (typeof window === 'undefined') return;
 
-    (function(w, d, s, l, i) {
+    (function (w: any, d: Document, s: string, l: string, i: string) {
       w[l] = w[l] || [];
       w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+
       const f = d.getElementsByTagName(s)[0],
         j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
+        dl = l !== 'dataLayer' ? '&l=' + l : '';
+
       j.async = true;
       j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode!.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-WN9GFB44');
+      f.parentNode?.insertBefore(j, f);
+    })(window as any, document, 'script', 'dataLayer', 'GTM-WN9GFB44');
   }, []);
 
   return null;
