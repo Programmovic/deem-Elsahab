4cb8bd78ae231fcc30c0af8ae195a90885c464d8
Adam Hasan Said <91834955+Programmovic@users.noreply.github.com>
2025-05-24 22:46:21 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import PageHeader from './components/PageHeader';
-import Head from 'next/head';
+import GTM from './components/GTM';
 
 export const metadata = {
   title: 'ديم السحاب',
@@ -16,20 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ar" dir="rtl">
-      <Head>
-        {/* Google Tag Manager Script */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-WN9GFB44');`,
-          }}
-        />
-      </Head>
       <body>
-        {/* Google Tag Manager (noscript) */}
+        <GTM />
+
+        {/* GTM noscript fallback */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-WN9GFB44"
